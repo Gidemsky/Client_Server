1a51138d6fb049f00fea78280db4ea0da5bfbbf0
Gideon <gidemsky26@gmail.com>
2019-01-16 15:19:51 +0200

    connection fixed

--- ParallelServer.h
+++ ParallelServer.h
@@ -8,7 +8,6 @@ class ParallelServer: public Server{
     int port{};
     IClientHandler* clientHandler{};
 public:
-    ParallelServer();
     void open(int port, IClientHandler* clientHandler) override;
     static void start(int, IClientHandler *);
     bool stop() override;
